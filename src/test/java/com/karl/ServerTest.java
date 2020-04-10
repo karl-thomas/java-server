d3fb90cc3d517a45870fe20a0c8d3a1edad05e2d
Karl Thomas <karl.thomas925@gmail.com>
2020-04-09 22:52:14 -0500

    write test for empty request, clean up tests

--- src/test/java/com/karl/ServerTest.java
+++ src/test/java/com/karl/ServerTest.java
@@ -4,33 +4,57 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.IOException;
 import com.karl.mocks.MockServerSocket;
 import com.karl.mocks.MockSocketWrapper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 @Tag("unit")
 public class ServerTest {
+  private MockSocketWrapper mocket;
+  private MockServerSocket serverSocket;
+  private Server server;
+
+  @BeforeEach
+  public void setupEach() throws IOException {
+    mocket = new MockSocketWrapper();
+    serverSocket = new MockServerSocket();
+    serverSocket.socket = mocket;
+    server = new Server(serverSocket);
+  }
+
+  @AfterEach
+  public void tearDownEach() throws IOException {
+    server.close();
+  }
+
   @Test
   @DisplayName("server can connect to a client socket")
   public void serverCanConnectToSocket() throws IOException {
-    MockSocketWrapper socket = new MockSocketWrapper();
-    MockServerSocket serverSocket = new MockServerSocket();
-    serverSocket.socket = socket;
+    assertEquals(server.accept(), mocket);
+  }
 
-    Server server = new Server(serverSocket);
+  @Test
+  @DisplayName("server will not write anything if the client didn't send anthing")
+  public void serverWillNotRespondToNothing() throws IOException {
+    String message = "";
+    mocket.textFromClient = message;
 
-    assertEquals(server.accept(), socket);
-    server.close();
+    serverSocket.closeAfterConnections(1);
+    server.start();
+
+    String result = mocket.sentToClient();
+
+    assertEquals(message, result);
   }
 
   @Test
   @DisplayName("server writes the same message, with a newline, to the client it was sent from")
   public void serverCanHandleARequest() throws IOException {
     String message = "hello there!";
-    MockSocketWrapper mocket = new MockSocketWrapper(message);
-    MockServerSocket serverSocket = new MockServerSocket();
-    serverSocket.socket = mocket;
-    Server server = new Server(serverSocket);
+    mocket.textFromClient = message;
 
     serverSocket.closeAfterConnections(1);
     server.start();
@@ -38,6 +62,5 @@ public class ServerTest {
     String result = mocket.sentToClient();
 
     assertEquals(message + "\n", result);
-    server.close();
   }
 }

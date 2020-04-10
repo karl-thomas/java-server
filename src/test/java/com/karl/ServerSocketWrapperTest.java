c1ff1f9663c195af8bf7a5f12c12ca97afae9c0d
Karl Thomas <karl.thomas925@gmail.com>
2020-04-09 23:13:30 -0500

    clean up syntax

--- src/test/java/com/karl/ServerSocketWrapperTest.java
+++ src/test/java/com/karl/ServerSocketWrapperTest.java
@@ -2,17 +2,15 @@ package com.karl;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.IOException;
-import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ServerSocketWrapperTest {
-  @Nested
-  class closeMethod {
-    @Test
-    public void shouldCloseInnerSocket() throws IOException {
-      ServerSocketWrapper socket = new ServerSocketWrapper(8080);
-      socket.close();
-      assertTrue(socket.isClosed());
-    }
+  @Test
+  @DisplayName("close should close the inner server socket")
+  public void close_shouldCloseInnerSocket() throws IOException {
+    ServerSocketWrapper socket = new ServerSocketWrapper(8080);
+    socket.close();
+    assertTrue(socket.isClosed());
   }
 }
--- src/test/java/com/karl/ServerTest.java
+++ src/test/java/com/karl/ServerTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.IOException;
 import com.karl.mocks.MockServerSocket;
 import com.karl.mocks.MockSocketWrapper;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;

25750d7249ecde6055b9eed273a1756b1e5fe796
Karl Thomas <karl.thomas925@gmail.com>
2020-04-15 13:50:56 -0500

    get basic terraform setup

---
+++ java-server.tf
@@ -0,0 +1,36 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+variable "server_port" {
+  description = "The port the server will use for requests"
+  default     = 7777
+}
+
+resource "aws_security_group" "websg" {
+  name = "java-server-websg"
+  ingress {
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "webserver" {
+  ami                    = "ami-db622fcc"
+  instance_type          = "t2.micro"
+  key_name               = "deploy-server"
+  vpc_security_group_ids = ["${aws_security_group.websg.id}"]
+
+  tags = {
+    Name = "java-server"
+  }
+}

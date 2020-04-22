0657a4e6a72f4c1c4caeba47554a8fbf90cc7ba0
Karl Thomas <karl.thomas925@gmail.com>
2020-04-22 02:45:16 -0500

    make a switch between http and echo

--- src/main/java/com/karl/Main.java
+++ src/main/java/com/karl/Main.java
@@ -2,6 +2,8 @@ package com.karl;
 
 import java.io.IOException;
 import com.karl.constants.Globals;
+import com.karl.echo.EchoServer;
+import com.karl.http.HTTPServer;
 import com.karl.wrappers.WrappedServerSocket;
 
 public class Main {
@@ -13,8 +15,20 @@ public class Main {
       port = Globals.PORT;
     }
 
+    String serverType;
+    try {
+      serverType = args[1];
+    } catch (Exception e) {
+      serverType = "http";
+    }
+
     WrappedServerSocket socket = new ServerSocketWrapper(port);
-    Server server = new Server(socket);
-    server.start();
+    if (serverType.equals("echo")) {
+      EchoServer server = new EchoServer(socket);
+      server.start();
+    } else if (serverType.equals("http")) {
+      HTTPServer server = new HTTPServer(socket);
+      server.start();
+    }
   }
 }
--- src/main/java/com/karl/http/HTTPserver.java
+++ src/main/java/com/karl/http/HTTPserver.java
@@ -1,5 +1,34 @@
 package com.karl.http;
 
-public class HTTPserver {
+import java.io.IOException;
+import com.karl.echo.EchoHandler;
+import com.karl.wrappers.WrappedServerSocket;
+import com.karl.wrappers.WrappedSocket;
 
+public class HTTPServer {
+  private final WrappedServerSocket serverSocket;
+  private WrappedSocket clientSocket;
+
+  public HTTPServer(WrappedServerSocket serverSocket) {
+    this.serverSocket = serverSocket;
+  }
+
+  public void start() throws IOException {
+    while (!serverSocket.isClosed()) {
+      clientSocket = accept();
+      EchoHandler handler = new EchoHandler(clientSocket);
+      new Thread(handler).start();
+    }
+  }
+
+  public WrappedSocket accept() throws IOException {
+    return serverSocket.accept();
+  }
+
+  public void close() throws IOException {
+    if (clientSocket != null)
+      clientSocket.close();
+    if (serverSocket != null)
+      serverSocket.close();
+  }
 }
